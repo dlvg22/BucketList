2f8af3829aa7676209220666f1f2210f9f6cb224
unknown <allancruz315@yahoo.com>
2017-09-29 16:16:37 +0800

    Improved Views for settings

--- application/views/bucket/settings.php
+++ application/views/bucket/settings.php
@@ -1,11 +1,127 @@
-
+        <div class="row">
         			<?php
         			$data=array('email'=>'dlvg22google.com','nickname'=>'edu', 'username'=>'eduardoeduardo'); //dummy data
 					?>
+          <div class="col-md-offset-2 col-md-8 contents">
+					<h2>Account Settings</h2></br>
+					<table class="table-responsive table">
+          <tr><td>
+					<div class="item">
+					<p><b>Nickname: </b>&nbsp;<u><?php echo $data['nickname']; ?></u><span class="glyphicon glyphicon-pencil righter"></span></p>
+					
+					<span class="menu">
+						<form class="form-horizontal">
+    					<div class="form-group">
+      					 	<label for="inputEmail" class="control-label col-xs-3">New Nickname</label>
+     					   	<div class="col-xs-6">
+            				<input type="email" class="form-control" id="inputEmail" placeholder="New Nickname">
+        					</div>
+    					  	<div class="col-xs-offset-3 col-xs-6s ">
+            				<button type="submit" class="btn btn-primary">
+            				<span class="glyphicon glyphicon-edit"></span> Change
+            				</button>
+        					<button class="btn btn-default closer">Cancel</button>
+        					</div>
+    					</div>
+					</form>
+					</span>
+					</div></td>
+		      </tr>
 
-					<div class="col-md-9 contents">
-					<p><b>Nickname: </b><?php echo $data['nickname']; ?></p>
-					<p><b>Username: </b><?php echo $data['username']; ?></p>		
-					<p><b>Email: </b><?php echo $data['email']; ?></p>
+          <tr><td>
+    			<div class="item">
+					<p><b>Username: </b>&nbsp;<u><?php echo $data['username']; ?></u><span class="glyphicon glyphicon-pencil righter"></span></p>		
+					<span class="menu">
+						<form class="form-horizontal">
+    					<div class="form-group">
+      					 	<label for="inputEmail" class="control-label col-xs-3">New Username</label>
+     					   	<div class="col-xs-6">
+            				<input type="email" class="form-control" id="inputEmail" placeholder="New Username">
+        					</div>
+    					   	<div class="col-xs-offset-3 col-xs-6s ">
+            				<button type="submit" class="btn btn-primary">
+            				<span class="glyphicon glyphicon-edit"></span> Change
+            				</button>
+                    <button class="btn btn-default closer">Cancel</button>
+        					</div>
+    					</div>
+					</form>
+					</span>
 					</div>
-				</div>
+					</td></tr>
+          <tr><td>
+          <div class="item">
+          <p><b>Email: </b>&nbsp;<u><?php echo $data['email']; ?></u><span class="glyphicon glyphicon-pencil righter"></span></p>
+        
+          <span class="menu">
+            <form class="form-horizontal">
+              <div class="form-group">
+                  <label for="inputEmail" class="control-label col-xs-3">New email</label>
+                  <div class="col-xs-6">
+                    <input type="email" class="form-control" id="inputEmail" placeholder="New Email">
+                  </div>
+                  <div class="col-xs-offset-3 col-xs-6s ">
+                    <button type="submit" class="btn btn-primary">
+                    <span class="glyphicon glyphicon-edit"></span> Change
+                    </button>
+                    <button class="btn btn-default closer">Cancel</button>
+                  </div>
+              </div>
+          </form>
+          </span>
+          </div></td>
+          </tr>
+          <tr><td>
+          <div class="item">
+					<p><b>Change Password </b><span class="glyphicon glyphicon-pencil righter"></span></p>		
+					<span class="menu">
+						<form class="form-horizontal">
+          				<div class="form-group">
+      					 	<label for="inputEmail" class="control-label col-xs-3">Old Password</label>
+     					   	<div class="col-xs-6">
+            				<input type="password" class="input-md form-control" id="inputEmail" placeholder="New Password">
+        					</div>
+    					</div>
+    					<div class="form-group">
+        					<label for="inputPassword" class="control-label col-xs-3">New Password</label>
+       						<div class="col-xs-6">
+    				        <input type="password" class="input-md form-control" id="inputPassword" placeholder="Retype Password">
+        					</div>
+    					</div>
+       					<div class="form-group">
+   					    	<div class="col-xs-offset-3 col-xs-6s ">
+            				<button type="submit" class="btn btn-primary">
+            				<span class="glyphicon glyphicon-edit"></span> Change
+            				</button><button class="btn btn-default closer">Cancel</button>
+        					</div>
+    					</div>
+					</form>
+					</span>
+					</div>
+				  
+          </td></tr>
+          </table>
+
+        </div>
+        </div>
+
+        <script>
+              $('.menu').hide();
+
+              $('.item p').click(function(e){
+
+              e.preventDefault();
+              var $this = $(this).parent().find('.menu');
+              $(".menu").not($this).hide();
+              $(".menu").not($this).parent().removeClass("colored");
+              $this.toggle();
+              $(this).parent().toggleClass("colored");
+              });
+
+              $('.closer').click(function(e){
+               e.preventDefault();
+              $(".menu").hide();
+              $(".item").removeClass("colored");
+              });
+              
+        </script>
--- application/views/template/header.php
+++ application/views/template/header.php
@@ -14,7 +14,7 @@
   <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  
+  <link rel="stylesheet" href="<?php echo base_url("assets/css/settings.css"); ?>">
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   		 <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
   <script src="<?php echo base_url('assets/js/jquery-3.2.1.min.js');?>"></script>
@@ -24,4 +24,5 @@
   <script src="<?php echo base_url('assets/js/script.js');?>"></script>
 	
 	</head>
+	<body>
 
