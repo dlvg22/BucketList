684e63fb466769c4a16e624d20bf1c6757a91ae6
BrixxCaridad <brixxcaridad14@gmail.com>
2017-09-30 13:20:53 +0800

    db

--- application/controllers/Bucket.php
+++ application/controllers/Bucket.php
@@ -2,6 +2,14 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Bucket extends CI_Controller {
+	
+		public function __construct()
+	{
+		parent:: __construct();
+		
+		$this->load->model('bucket_model','dbBucket');
+		
+	}
 
 	public function index()
 	{
