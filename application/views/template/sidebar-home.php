9412c4f2b73053ead57c558fccffd41c3a0df91d
BrixxCaridad <brixxcaridad14@gmail.com>
2017-10-17 02:29:22 +0800

    updated photo slider

--- application/views/template/sidebar-home.php
+++ application/views/template/sidebar-home.php
@@ -1,22 +1,104 @@
-<!-- home photo -->
-<div class="row sidebarprofile">
-<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 col-xl-2 default-space bigheader">
-			<div class="photo-template ">
-			<div ><a href=""><img src="<?php echo base_url('assets/images/franky.jpg'); ?>" class="user-image side-profilepict" /></a></div>
-			
-			<?php
+<script>
+function openModal() {
+  document.getElementById('myModal').style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+</script>
+<?php
 			$name="Frank Ho";
 			$user="@papaFrank";
 			$about="!the algo you've usually see;";
 			$location="Tondo,Manila Philippines";
 			$birthday="April 22,1998";
 			$work="Programmer/developer";
-			
-			echo '<p class="user-name">".$user_profile['first_name']."</p>';
-			echo '<p class="user-acc">'.$user.'</p>';
-			echo '<p>Facebook URL : "."<a href=".$user_profile['link']." target='_blank'"."> https://www.facebook.com/".$user_profile['id']."</a></p>';
+		
 
 			?>
+<!-- home photo -->
+<div class="row sidebarprofile">
+<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2 col-xl-2 default-space bigheader">
+			<div class="photo-template ">
+			<div ><img style="width:100%" onclick="openModal();currentSlide(1)" class="hover-shadow cursor" src="<?php echo base_url('assets/images/franky.jpg'); ?>" class="user-image side-profilepict" /></div>
+			
+			<div id="myModal" class="modal">
+			<span class="close cursor" onclick="closeModal()">&times;</span>
+  <div class="modal-content">
+
+    <div class="mySlides">
+      <div class="numbertext"></div>
+      <img src="<?php echo base_url('assets/images/franky.jpg'); ?>" style="width:100%" class="img-size">
+    </div>
+
+    <div class="mySlides">
+      <div class="numbertext"></div>
+      <img src="<?php echo base_url('assets/images/luffy1.jpg'); ?>" style="width:100%" class="img-size">
+    </div>
+
+    <div class="mySlides">
+      <div class="numbertext"></div>
+      <img src="<?php echo base_url('assets/images/luffy2.jpg'); ?>" style="width:100%" class="img-size">
+    </div>
+    
+    <div class="mySlides">
+      <div class="numbertext"></div>
+      <img src="<?php echo base_url('assets/images/luffy3.jpg'); ?>" style="width:100%" class="img-size">
+    </div>
+    
+    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+    <div class="caption-container">
+      <p id="caption"></p>
+    </div>
+
+
+    <div class="column">
+      <img class="demo cursor" src="<?php echo base_url('assets/images/franky.jpg'); ?>" style="width:100%" onclick="currentSlide(1)" alt="<?php echo $name; ?>">
+    </div>
+    <div class="column">
+      <img class="demo cursor" src="<?php echo base_url('assets/images/luffy1.jpg'); ?>" style="width:100%" onclick="currentSlide(2)" alt="<?php echo $name; ?>">
+    </div>
+    <div class="column">
+      <img class="demo cursor" src="<?php echo base_url('assets/images/luffy2.jpg'); ?>" style="width:100%" onclick="currentSlide(3)" alt="<?php echo $name; ?>">
+    </div>
+    <div class="column">
+      <img class="demo cursor" src="<?php echo base_url('assets/images/luffy3.jpg'); ?>" style="width:100%" onclick="currentSlide(4)" alt="<?php echo $name; ?>">
+    </div>
+  </div>
+</div>
+			
 			</div>
 
 
@@ -31,7 +113,7 @@
 			<p class="specialfooter"><br>&copy;BucketList <a href="">About Us</a> <a href="">Developers</a> <a href="">Credits</a></p>
 		
 		</div>
-
+ 
 		   
 
 	</div>    
